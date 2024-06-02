0c23ddc5233b90a535c5cbe08fba42b1130e57d1
jcorporation <mail@jcgames.de>
2024-06-03 01:38:17 +0200

    Upd: some improvements

--- Misc/PlayRandomPlaylist.lua
+++ Misc/PlayRandomPlaylist.lua
@@ -6,16 +6,22 @@ local rc, result = mympd.api("MYMPD_API_PLAYLIST_LIST", {
     searchstr = "",
     type = 0
 })
+if rc == 1 then
+    return "Failure getting playlists"
+end
 -- random number
 math.randomseed(os.time())
-local number = math.random(1, #result.result.data)
+local number = math.random(1, #result.data)
 -- get playlist by random number
 local playlist = result.data[number].uri
 -- play the playlist
-local raw_result
-rc, raw_result = mympd.api("MYMPD_API_QUEUE_REPLACE_PLAYLISTS", {
+rc, result = mympd.api("MYMPD_API_QUEUE_REPLACE_PLAYLISTS", {
     plist = { playlist },
     play = true
 })
--- return the playlist name
-return "Playing " .. playlist
+if rc == 0 then
+    -- return the playlist name
+    return "Playing " .. playlist
+else
+    return "Failure playing " .. playlist
+end
