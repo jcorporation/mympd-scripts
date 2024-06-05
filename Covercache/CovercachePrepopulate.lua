c1ca66fd924340c9adc432a0e93fcc3490c3294d
Juergen <juergen.mang@axians.de>
2024-06-05 21:59:12 +0200

    Feat: Add script to prepopulate the myMPD covercache

---
+++ Covercache/CovercachePrepopulate.lua
@@ -0,0 +1,65 @@
+-- {"order":1,"arguments":[]}
+
+mympd.init()
+
+local extensions = {
+    "webp",
+    "jpg",
+    "jpeg",
+    "png",
+    "avif"
+}
+
+local function file_exists(name)
+    local f = io.open(name,"r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    end
+    return false
+end
+
+local function check_image(base)
+    for _, ext in ipairs(extensions) do
+        local p = base .. "." .. ext
+        if file_exists(p) then
+            return true
+        end
+    end
+    return false
+end
+
+local rc, result = mympd.api("MYMPD_API_DATABASE_ALBUM_LIST", {
+    offset = 0,
+    limit = 10000,
+    expression = "",
+    sort = "Title",
+    sortdesc = false,
+    fields = {}
+})
+
+local existing = 0
+local errors = 0
+local downloaded = 0
+for _, album in pairs(result.data) do
+    local path = mympd_env.cachedir_cover .. "/" .. mympd.hash_sha1(album.uri) .. "-0"
+    if not check_image(path) then
+        if not pcall(function()
+            local out = mympd.tmp_file()
+            local uri = mympd_state.mympd_uri .. 'albumart-thumb?offset=0&uri=' .. mympd.urlencode(album.uri)
+            if mympd_http_download(uri, out) == 0 then
+                local name = mympd.covercache_write(out, album.uri)
+                mympd.log(6, "Covercache: " .. name)
+                downloaded = downloaded + 1
+            else
+                errors = errors + 1
+            end
+        end) then
+            errors = errors + 1
+        end
+    else
+        existing = existing + 1
+    end
+end
+
+return "Existing: " .. existing .. ", Downloaded: " .. downloaded .. ", Errors: " .. errors
---
+++ Covercache/README.md
@@ -0,0 +1,5 @@
+# Covercache
+
+## CovercachePrepopulate
+
+This scripts downloads abumart from myMPD and stores it in the covercache.
