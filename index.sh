69281bb7194f20e75f526cda10db4198ba996e99
jcorporation <mail@jcgames.de>
2024-06-10 20:24:13 +0200

    Upd: add more metadata

--- index.sh
+++ index.sh
@@ -18,9 +18,16 @@ I=0
 for F in */*.lua
 do
     [ "$I" -gt 0 ] &&  printf "," >&3
-    CATEGORY=$(dirname "$F")
-    SCRIPTNAME=$(basename "$F")
-    printf "\"%s/%s\"" "$CATEGORY" "$SCRIPTNAME" >&3
+    NAME=$(basename "$F" .lua)
+    SCRIPT_HEADER=$(head -1 "$F" < $F | sed 's/^-- //')
+    DESC=$(printf "%s" "$SCRIPT_HEADER" | jq -r '.desc')
+    [ "$DESC" = "null" ] && DESC=""
+    jq -n --arg file "$F" --arg name "$NAME" --arg desc "$DESC" \
+        '{
+            "file": $file,
+            "name": $name,
+            "desc": $desc
+        }' >&3
     I=$((I+1))
 done
 printf "]}\n" >&3
