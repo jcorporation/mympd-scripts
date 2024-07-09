90bf0ffbf69320d9047c8a9c6ff8093a12f584b7
jcorporation <mail@jcgames.de>
2024-07-09 21:30:34 +0200

    Fix: Ignore reference song from blissify output and request n + 1 songs
    Blissify issue: https://github.com/Polochon-street/blissify-rs/issues/66

--- Jukebox/JukeboxBlissify.lua
+++ Jukebox/JukeboxBlissify.lua
@@ -1,4 +1,4 @@
--- {"name": "JukeboxBlissify", "file": "Jukebox/JukeboxBlissify.lua", "version": 5, "desc": "Uses blissify-rs to populate the jukebox queue.", "order":0,"arguments":["addToQueue"]}
+-- {"name": "JukeboxBlissify", "file": "Jukebox/JukeboxBlissify.lua", "version": 6, "desc": "Uses blissify-rs to populate the jukebox queue.", "order":0,"arguments":["addToQueue"]}
 local blissify_path = mympd_env.var_blissify_path
 local blissify_config = ""
 if mympd_env.var_blissify_config ~= nil and
@@ -30,7 +30,8 @@ if rc == 1 then
     return
 end
 local jukebox_length = result.totalEntities
-local to_add = addSongs + min_jukebox_length - jukebox_length
+-- request one more song (https://github.com/Polochon-street/blissify-rs/issues/66)
+local to_add = addSongs + min_jukebox_length - jukebox_length + 1
 local last_song = nil
 if jukebox_length > 0 then
     last_song = result.data[jukebox_length].uri
@@ -70,15 +71,20 @@ local prefix_len = #mympd_state.music_directory + 2
 local songs = {}
 local cmd = string.format("%s %s playlist --seed-song --dry-run --from-song \"%s\" %d 2>/dev/null", blissify_path, blissify_config, full_song_path, to_add)
 local output = mympd.os_capture(cmd)
+-- Skip reference song
+local i = 0
 for line in string.gmatch(output, "[^\n]+") do
-    table.insert(songs, string.sub(line, prefix_len))
+    if i > 1 then
+        table.insert(songs, string.sub(line, prefix_len))
+    end
+    i = i + 1
 end
 
 if mympd_arguments.addToQueue == "1" then
     -- Add addSongs entries from playlist to the MPD queue
     local addUris = {}
-    for i = 1, addSongs do
-        table.insert(addUris, songs[i])
+    for j = 1, addSongs do
+        table.insert(addUris, songs[j])
     end
     rc, result = mympd.api("MYMPD_API_QUEUE_APPEND_URIS", {
         uris = addUris,
@@ -96,8 +102,8 @@ end
 
 -- Add songs to the jukebox queue
 local addUris = {}
-for i = addSongs, #songs do
-    table.insert(addUris, songs[i])
+for j = addSongs, #songs do
+    table.insert(addUris, songs[j])
 end
 rc, result = mympd.api("MYMPD_API_JUKEBOX_APPEND_URIS", {
     uris = addUris
