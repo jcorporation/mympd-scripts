52aee189df20dfbf9a5592cab097e9d1834479d4
jcorporation <mail@jcgames.de>
2025-04-13 19:42:28 +0200

    Upd: Background - Add fallback to albumart

--- Background/Background.lua
+++ Background/Background.lua
@@ -1,17 +1,19 @@
 -- {"name": "Background", "file": "Background/Background.lua", "version": 1, "desc": "Fetches a background image on demand.", "order": 0, "arguments": ["uri","hash"]}
 local providers = require "scripts/BackgroundProviders"
 
+local fallback_uri = "/albumart?offset=0&uri=" .. mympd.urlencode(uri)
+
 -- Get the song details
 local rc, song = mympd.api("MYMPD_API_PLAYER_CURRENT_SONG", {})
 if rc ~= 0 then
-    mympd.log(3, "No current song")
-    return mympd.http_redirect("/assets/coverimage-transparent")
+    mympd.log(6, "No current song")
+    return mympd.http_redirect(fallback_uri)
 end
 
 local out = mympd.tmp_file()
 if out == nil then
     mympd.log(3, "Failure creating tmp file.")
-    return mympd.http_redirect("/assets/coverimage-transparent")
+    return mympd.http_redirect(fallback_uri)
 end
 
 rc = 1
@@ -25,7 +27,7 @@ end
 
 if rc == 1 then
     os.remove(out);
-    return mympd.http_redirect("/assets/coverimage-transparent")
+    return mympd.http_redirect(fallback_uri)
 end
 
 return mympd.http_serve_file_rm(out)
