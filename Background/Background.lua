fe970c1efa1cb0b493134efe1367f52058e981eb
jcorporation <mail@jcgames.de>
2025-04-15 00:31:13 +0200

    Fix: fallback_uri

--- Background/Background.lua
+++ Background/Background.lua
@@ -1,7 +1,7 @@
 -- {"name": "Background", "file": "Background/Background.lua", "version": 1, "desc": "Fetches a background image on demand.", "order": 0, "arguments": ["uri","hash"]}
 local providers = require "scripts/BackgroundProviders"
 
-local fallback_uri = "/albumart?offset=0&uri=" .. mympd.urlencode(uri)
+local fallback_uri = "/albumart?offset=0&uri=" .. mympd.urlencode(mympd_arguments.uri)
 
 -- Get the song details
 local rc, song = mympd.api("MYMPD_API_PLAYER_CURRENT_SONG", {})
