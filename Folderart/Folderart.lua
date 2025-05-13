33211a0e1ae2cda8c3b13d266ab57ceab4f6f1a2
jcorporation <mail@jcgames.de>
2025-05-13 23:50:44 +0200

    Feat: Add Folderart script

---
+++ Folderart/Folderart.lua
@@ -0,0 +1,28 @@
+-- {"name": "Folderart", "file": "Folderart/Folderart.lua", "version": 1, "desc": "Creates folderart on demand.", "order": 0, "arguments": ["path"]}
+
+local rc, msg = mympd.check_arguments({path = "notempty"})
+if rc == false then
+    return mympd.http_redirect("/assets/coverimage-notavailable")
+end
+
+-- Get first song
+local result
+rc, result = mympd.api("MYMPD_API_DATABASE_FILESYSTEM_LIST", {
+    offset = 0,
+    limit = 1,
+    path = mympd_arguments.path,
+    type = "dir",
+    searchstr = "",
+    fields = {}
+})
+if rc ~= 0 then
+    mympd.log(3, "Folder not found")
+    return mympd.http_redirect("/assets/coverimage-notavailable")
+end
+
+if result.data[1] == nil then
+    mympd.log(3, "Folder is empty")
+    return mympd.http_redirect("/assets/coverimage-notavailable")
+end
+
+return mympd.http_redirect("/albumart?offset=0&uri=" .. mympd.urlencode(result.data[1].uri))
---
+++ Folderart/README.md
@@ -0,0 +1,10 @@
+# Folderart
+
+This script creates folderart by fetching the first song of the folder.
+
+## Usage
+
+1. Import the `Folderart.lua`.
+2. Create a new trigger
+    - Event: `mympd_folderart`
+    - Script: above script
