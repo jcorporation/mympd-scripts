138852e2f4226678c405d1d9c587fa63f68672a6
jcorporation <mail@jcgames.de>
2026-01-06 12:41:21 +0100

    Fix: Create index only if private key is found

--- index.sh
+++ index.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
 #SPDX-License-Identifier: GPL-3.0-or-later
-#myMPD (c) 2018-2024 Juergen Mang <mail@jcgames.de>
-#https://github.com/jcorporation/mympd
+#myMPD (c) 2018-2026 Juergen Mang <mail@jcgames.de>
+#https://github.com/jcorporation/mympd-sciprts
 
-#exit on error
+# Exit on error
 set -e
 
-#exit on undefined variable
+# Exit on undefined variable
 set -u
 
 sig_create() {
@@ -24,38 +24,49 @@ sig_check() {
     rm /tmp/sig
 }
 
-# Create index of lua scripts
-rm -f "index.json"
-exec 3<> "index.json"
-printf "{" >&3
-I=0
-for F in */*.lua
-do
-    echo -n "$F..."
-    [ "$I" -gt 0 ] &&  printf "," >&3
-    NAME=$(basename "$F" .lua | jq -Ra .)
-    FILE=$(printf "%s" "$F" | jq -Ra .)
-    SCRIPT_HEADER=$(head -1 "$F" < $F | sed 's/^-- //')
-    DESC=$(printf "%s" "$SCRIPT_HEADER" | jq -r '.desc' | jq -Ra .)
-    VERSION=$(printf "%s" "$SCRIPT_HEADER" | jq -r '.version')
-    [ "$DESC" = "null" ] && DESC=""
-    [ "$VERSION" = "null" ] && VERSION="0"
-    printf '%s:{"name":%s,"desc":%s,"version":%s}' "$FILE" "$NAME" "$DESC" "$VERSION">&3
-    I=$((I+1))
-    if [ -f privatekey.pem ]
-    then
-        sig_create "$F"
-    fi
-    sig_check "$F"
-done
-printf "}\n" >&3
-exec 3>&-
-
-# Check and sign index
-jq "." < index.json > /dev/null
+# Create signatures and index of lua scripts
 if [ -f privatekey.pem ]
 then
+    echo "--"
+    echo "Creating index and signatures"
+    rm -f "index.json"
+    exec 3<> "index.json"
+    printf "{" >&3
+    I=0
+    for F in */*.lua
+    do
+        echo "    $F"
+        [ "$I" -gt 0 ] &&  printf "," >&3
+        NAME=$(basename "$F" .lua | jq -Ra .)
+        FILE=$(printf "%s" "$F" | jq -Ra .)
+        SCRIPT_HEADER=$(head -1 "$F" < $F | sed 's/^-- //')
+        DESC=$(printf "%s" "$SCRIPT_HEADER" | jq -r '.desc' | jq -Ra .)
+        VERSION=$(printf "%s" "$SCRIPT_HEADER" | jq -r '.version')
+        [ "$DESC" = "null" ] && DESC=""
+        [ "$VERSION" = "null" ] && VERSION="0"
+        printf '%s:{"name":%s,"desc":%s,"version":%s}' "$FILE" "$NAME" "$DESC" "$VERSION" >&3
+        I=$((I+1))
+        if [ -f privatekey.pem ]
+        then
+            sig_create "$F"
+        fi
+    done
+    printf "}\n" >&3
+    exec 3>&-
+    # Sign index
     sig_create index.json
 fi
-echo -n "index.json..."
+
+# Check index for valid JSON
+jq "." < index.json > /dev/null
+
+echo "--"
+echo "Checking signatures"
+echo -n "    index.json..."
 sig_check index.json
+
+for F in */*.lua
+do
+    echo -n "    $F..."
+    sig_check "$F"
+done
