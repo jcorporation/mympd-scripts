6fe38d1fa6ede946b9e975f92213f63899b67896
ksz <22708888+ksz16@users.noreply.github.com>
2026-02-26 23:40:07 +0100

    Fix: Prevent crash when AlbumArtist or other tags are missing

--- last.fm/lastfm.lua
+++ last.fm/lastfm.lua
@@ -1,4 +1,4 @@
--- {"name": "lastfm", "file": "last.fm/lastfm.lua", "version": 4, "desc": "Interface for last.fm.", "order":0, "arguments":["trigger"]}
+-- {"name": "lastfm", "file": "last.fm/lastfm.lua", "version": 5, "desc": "Interface for last.fm.", "order":0, "arguments":["trigger"]}
 
 if mympd.isnilorempty(mympd_env.var.lastfm_api_key) then
   return mympd.jsonrpc_error("No Last.fm API Key set")
@@ -12,50 +12,61 @@ if rc == false then
     return msg
 end
 
+local function firstTagValue(field)
+  return (field and field[1]) or ""
+end
+
 local function hashRequest(data, secret)
   local keys = {}
   for key in pairs(data) do
     table.insert(keys, key)
   end
+
   table.sort(keys)
+
   local s = ""
-  for k, v in pairs(keys) do
-    s = s .. v .. data[v]
+  for _, key in ipairs(keys) do
+    s = s .. key .. data[key]
   end
+
   s = s .. secret
-  local hash = mympd.hash_md5(s)
-  return hash
+  return mympd.hash_md5(s)
 end
 
 local function urlencode_data(data)
-  local s = ""
   local a = {}
   for k, v in pairs(data) do
     table.insert(a, mympd.urlencode(k) .. "=" .. mympd.urlencode(v))
   end
-  s = table.concat(a, "&")
- return s
+  return table.concat(a, "&")
 end
 
 local function sendData(data)
   local extra_headers = 'Content-type: application/x-www-form-urlencoded\r\n'
-  local hash = hashRequest(data, mympd_env.var.lastfm_secret)
-  data["api_sig"] = hash
-  data = urlencode_data(data)
+  data["api_sig"] = hashRequest(data, mympd_env.var.lastfm_secret)
+  local encoded = urlencode_data(data)
+
   local code, headers, body
-  rc, code, headers, body = mympd.http_client("POST", "https://ws.audioscrobbler.com/2.0/?format=json", extra_headers, data)
+  rc, code, headers, body = mympd.http_client(
+    "POST",
+    "https://ws.audioscrobbler.com/2.0/?format=json",
+    extra_headers,
+    encoded
+  )
+
   return rc, body
 end
 
--- main
+-- MAIN
 mympd.init()
 
+-- NOW PLAYING
 if mympd_arguments.trigger == "player" then
   if mympd_state.play_state ~= 2 or mympd_state.elapsed_time > 5 then
     return "Now Playing: Not Playing"
   end
 
-  if mympd_state.current_song == nil then
+  if not mympd_state.current_song then
     return "Now Playing: Not Playing"
   end
 
@@ -64,17 +75,17 @@ if mympd_arguments.trigger == "player" then
     return "webradio"
   end
 
-  if mympd.tblvalue_in_list(mympd_env.var.scrobble_genre_blacklist, mympd_state.current_song.Genre) == true then
+  if mympd.tblvalue_in_list(mympd_env.var.scrobble_genre_blacklist, mympd_state.current_song.Genre) then
     return
   end
 
   local data = {
     method      = "track.updateNowPlaying",
     api_key     = mympd_env.var.lastfm_api_key,
-    track       = mympd_state.current_song.Title,
-    artist      = mympd_state.current_song.Artist[1],
-    album       = mympd_state.current_song.Album,
-    albumArtist = mympd_state.current_song.AlbumArtist[1],
+    track       = mympd_state.current_song.Title or "",
+    artist      = firstTagValue(mympd_state.current_song.Artist),
+    album       = mympd_state.current_song.Album or "",
+    albumArtist = firstTagValue(mympd_state.current_song.AlbumArtist),
     sk          = mympd_env.var.lastfm_session_key,
   }
 
@@ -86,19 +97,21 @@ if mympd_arguments.trigger == "player" then
 
   local ret = json.decode(body)
   local code = ret.nowplaying.ignoredMessage.code
+
   if code ~= "0" then
-    return "Now Playing: " .. mympd_state.current_song.Artist[1] .. " - " .. mympd_state.current_song.Title .. " - ignored code " .. code
+    return "Now Playing: " .. data.artist .. " - " .. data.track .. " - ignored code " .. code
   end
 
-  return "Now Playing: " .. mympd_state.current_song.Artist[1] .. " - " .. mympd_state.current_song.Title .. " - OK"
+  return "Now Playing: " .. data.artist .. " - " .. data.track .. " - OK"
 end
 
+-- SCROBBLE
 if mympd_arguments.trigger == "scrobble" then
-  if mympd_state.current_song == nil then
+  if not mympd_state.current_song then
     return "Scrobble: Not playing"
   end
 
-  if mympd.tblvalue_in_list(mympd_env.var.scrobble_genre_blacklist, mympd_state.current_song.Genre) == true then
+  if mympd.tblvalue_in_list(mympd_env.var.scrobble_genre_blacklist, mympd_state.current_song.Genre) then
     return
   end
 
@@ -106,10 +119,10 @@ if mympd_arguments.trigger == "scrobble" then
     method      = "track.scrobble",
     api_key     = mympd_env.var.lastfm_api_key,
     timestamp   = tostring(mympd_state.start_time),
-    track       = mympd_state.current_song.Title,
-    artist      = mympd_state.current_song.Artist[1],
-    album       = mympd_state.current_song.Album,
-    albumArtist = mympd_state.current_song.AlbumArtist[1],
+    track       = mympd_state.current_song.Title or "",
+    artist      = firstTagValue(mympd_state.current_song.Artist),
+    album       = mympd_state.current_song.Album or "",
+    albumArtist = firstTagValue(mympd_state.current_song.AlbumArtist),
     sk          = mympd_env.var.lastfm_session_key,
   }
 
@@ -121,13 +134,15 @@ if mympd_arguments.trigger == "scrobble" then
 
   local ret = json.decode(body)
   local code = ret.scrobbles.scrobble.ignoredMessage.code
+
   if code ~= "0" then
-    return "Scrobble: " .. mympd_state.current_song.Artist[1] .. " - " .. mympd_state.current_song.Title .. " ignored, code " .. code
+    return "Scrobble: " .. data.artist .. " - " .. data.track .. " ignored, code " .. code
   end
 
-  return "Scrobble: " .. mympd_state.current_song.Artist[1] .. " - " .. mympd_state.current_song.Title .. " OK"
+  return "Scrobble: " .. data.artist .. " - " .. data.track .. " OK"
 end
 
+-- FEEDBACK
 if mympd_arguments.trigger == "feedback" then
   if mympd_arguments.type == "like" then
     if mympd_arguments.vote == "1" then
@@ -139,15 +154,15 @@ if mympd_arguments.trigger == "feedback" then
     end
   end
 
-  if mympd_state.current_song == nil then
+  if not mympd_state.current_song then
     return "Feedback: Not playing"
   end
 
   local data = {
     method      = "track.love",
     api_key     = mympd_env.var.lastfm_api_key,
-    track       = mympd_state.current_song.Title,
-    artist      = mympd_state.current_song.Artist[1],
+    track       = mympd_state.current_song.Title or "",
+    artist      = firstTagValue(mympd_state.current_song.Artist),
     sk          = mympd_env.var.lastfm_session_key,
   }
 
@@ -157,18 +172,20 @@ if mympd_arguments.trigger == "feedback" then
     return "Feedback: Error"
   end
 
-  return "Feedback: " .. mympd_state.current_song.Artist[1] .. " - " .. mympd_state.current_song.Title .. " OK"
+  return "Feedback: " .. data.artist .. " - " .. data.track .. " OK"
 end
 
+-- GET SESSION KEY DIALOG
 if mympd_arguments.trigger == "key" then
   local data = {
     { name = "Username", type = "text", value = "" },
-    { name = "Password", type = "password", value = ""},
-    { name = "trigger", type = "hidden", value = "fetchkey"}
+    { name = "Password", type = "password", value = "" },
+    { name = "trigger",  type = "hidden", value = "fetchkey" }
   }
-  return mympd.dialog("Get last.fm key", data, "lastfm")
+  return mympd.dialog("Get Last.fm key", data, "lastfm")
 end
 
+-- FETCH SESSION KEY
 if mympd_arguments.trigger == "fetchkey" then
   local data = {
     method   = "auth.getMobileSession",
@@ -180,7 +197,11 @@ if mympd_arguments.trigger == "fetchkey" then
   local body
   rc, body = sendData(data)
   local ret = json.decode(body)
-  mympd.api("MYMPD_API_SCRIPT_VAR_SET", { key = "lastfm_session_key", value = ret.session.key })
+
+  mympd.api("MYMPD_API_SCRIPT_VAR_SET", {
+    key = "lastfm_session_key",
+    value = ret.session.key
+  })
 
   return "Set session key for last.fm to " .. ret.session.key
 end
